9bba5b5f9224c61e1261eb219cf72f7778b27c10
GabeCampbell1010 <gabecampbell1010@gmail.com>
2017-08-28 20:01:06 -0600

    Add project files.

---
+++ WindowsFormsApplication15/BinaryConversion.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication15
+{
+    class BinaryConversion
+    {
+        //static void Main(string[] args)
+        //{
+        //    string binaryNumberToConvert;
+        //    Console.WriteLine("Enter the binary number you would like to see the signed and unsigned decimal values of");
+        //    binaryNumberToConvert = Console.ReadLine();
+
+        //    Console.WriteLine("the unsigned binary number {0} has a decimal value of {1}", binaryNumberToConvert, GetUnsignedBinaryValue(binaryNumberToConvert));
+
+        //    Console.WriteLine("the signed binary number {0} has a decimal value of {1}", binaryNumberToConvert, GetSignedBinaryValue(binaryNumberToConvert));
+
+        //    Console.ReadKey();
+        //}
+
+        //unsigned
+        public static int GetUnsignedBinaryValue(string binaryString)
+        {
+            int[] array = new int[binaryString.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = binaryString[i] - '0';//subtract the ascii character '0' so that "0" and "1" become the integers 0 and 1 and not 48 and 49 
+            }
+
+            double sum = 0;
+            double temp;
+            int exponent = 0;
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                temp = (array[i] * Math.Pow(2, exponent++));
+                sum += temp;
+            }
+            return (int)sum;
+        }
+
+        //helper method for signed
+        public static bool CheckForTwos(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //signed
+        public static int GetSignedBinaryValue(string binaryString)
+        {
+            int[] array = new int[binaryString.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = binaryString[i] - '0';
+            }
+            bool isNegative = false;
+            if (array[0] == 1)//that is, if this is a negative binary number
+            {
+                isNegative = true; //Console.WriteLine(isNegative);
+                for (int i = 0; i < array.Length; i++)//flip the values
+                {
+                    if (array[i] == 0)
+                        array[i] = 1;
+                    else
+                        array[i] = 0;
+                }
+                //add 1 to the final position in the array
+                array[array.Length - 1] += 1;
+
+                //check if adding that above 1 in the last spot of the array has increased the value to 2, and adjust the array accordingly so long as it is not in proper binary format
+                while (CheckForTwos(array))
+                {
+                    for (int i = 0; i < array.Length - 1; i++)
+                    {
+                        if (array[i] > 1)
+                        {
+                            array[i] = 1;
+                            array[i - 1] += 1;
+                        }
+                    }
+                }
+            }
+
+            double sum = 0;
+            double temp;
+            int exponent = 0;
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                temp = (array[i] * Math.Pow(2, exponent++));
+                sum += temp;
+            }
+            return (int)(sum * (isNegative ? -1 : 1));
+        }
+    }
+}
---
+++ WindowsFormsApplication15/BinaryConverter.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication15
+{
+    public partial class BinaryConverter : Form
+    {
+        public BinaryConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string startupPath = Application.StartupPath;
+            string file = startupPath + @"\binaryFile";
+
+            StreamWriter writer = new StreamWriter(file);
+
+            while (true)
+            {
+                string response = "";
+                do
+                {
+                    response = Microsoft.VisualBasic.Interaction.InputBox("enter a new binary number?", "New Number?", "", 200, 200);
+                }
+                while (response != "y" && response != "n");
+
+                if (response == "n")
+                    break;
+
+                string binaryNumber;
+                do {
+                    binaryNumber = Microsoft.VisualBasic.Interaction.InputBox("Type the new number in Binary", "Binary Number", "", 200, 200);
+                    binaryNumber = binaryNumber.Trim();
+                }
+                while (!Regex.IsMatch(binaryNumber, "^[01]+$"));
+
+                string signed = "";
+                do {
+                    signed = Microsoft.VisualBasic.Interaction.InputBox("Would you like the signed binary decimal? 'y' for signed and 'n' for unsigned.", "Signed or Unsigned", "", 200, 200);
+                }
+                while (signed != "y" && signed != "n");
+
+
+                writer.WriteLine(binaryNumber);
+                writer.WriteLine(signed);
+            }
+
+            writer.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string startupPath = Application.StartupPath;
+            string file = startupPath + @"\binaryFile";
+
+            StreamReader reader = new StreamReader(file);
+
+            string contents = "";
+            string binaryNumber;
+            string signed;
+            bool isSigned;
+            
+
+            while (true)
+            {
+                
+                binaryNumber = reader.ReadLine();
+                signed = reader.ReadLine();
+                isSigned = signed == "y" ? true : false;
+
+                if (binaryNumber == null)
+                    break;
+                
+
+
+                if (isSigned)
+                {
+                    
+                    contents += "The signed binary number " + binaryNumber + " in decimal form is: " + BinaryConversion.GetSignedBinaryValue(binaryNumber).ToString() + Environment.NewLine;
+                }
+                else if(!isSigned)
+                {
+                    contents += "The unsigned binary number " + binaryNumber + " in decimal form is: " + BinaryConversion.GetUnsignedBinaryValue(binaryNumber).ToString() + Environment.NewLine;
+                }
+                
+            }
+
+            reader.Close();
+
+            textBox1.Text = contents;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
